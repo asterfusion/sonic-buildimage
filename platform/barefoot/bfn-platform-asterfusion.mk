2f7eea163b4dfde58e1fa2cfe2bf5b3b1f7f5dbb
SunZheng <sunzheng@asterfusion.com>
2024-09-25 18:22:56 +0800

    [Platform] Fix syncd build issue due incorrect platform makefile.
    Signed-off-by: SunZheng <sunzheng@asterfusion.com>

--- platform/barefoot/bfn-platform-asterfusion.mk
+++ platform/barefoot/bfn-platform-asterfusion.mk
@@ -1,5 +1,5 @@
-BFN_INGRASYS_PLATFORM = bfnplatform-asterfusion-master_1.0.0_amd64.deb
-$(BFN_INGRASYS_PLATFORM)_URL = "https://github.com/asterfusion/bf-bsp-lts/releases/download/24.06/bfnplatform-asterfusion-master_1.0.0_amd64.deb"
+BFN_ASTERFUSION_PLATFORM = bfnplatform-asterfusion-master_1.0.0_amd64.deb
+$(BFN_ASTERFUSION_PLATFORM)_URL = "https://github.com/asterfusion/bf-bsp-lts/releases/download/24.06/bfnplatform-asterfusion-master_1.0.0_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_ASTERFUSION_PLATFORM) # $(BFN_SAI_DEV)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_ASTERFUSION_PLATFORM)
